c5661165f235a8282e93b114b24d7adc5d84a50d
ishaanyadav007 <yadav.ishaan@gmail.com>
2026-05-22 15:05:24 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,46 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # m1: bruteforce of 2 loops
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        # m2: sorting and two pointer approach
+        # A=[]
+        # for i,num in enumerate(nums):
+        #     A.append([num,i])           # ** REV: This order is imp as sorting is done on the basis of first number in 2D lists
+        
+        # A.sort() 
+        # i, j = 0, len(nums) - 1
+        # while i<j:
+        #     curr = A[i][0] + A[j][0]
+        #     if curr == target:
+        #         return [min(A[i][1],A[j][1]),max(A[i][1],A[j][1])]
+        #     elif curr > target:
+        #         j-=1
+        #     else:
+        #         i+=1
+
+        #m3: 
+        A=[]
+        for i,num in enumerate(nums):
+            A.append([num,i])
+        A.sort()
+        i=0;j=len(nums)-1
+        while i<j:
+            curr = A[i][0] + A[j][0]
+            if curr==target:
+                return [min(A[i][1],A[j][1]),max(A[i][1],A[j][1])]
+            elif curr < target:
+                i+=1
+            else:
+                j-=1
+
+
+
+
+
+
+
+
