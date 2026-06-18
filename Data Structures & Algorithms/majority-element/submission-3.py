be7298d0aa0468d2b8c153b78c7357fc817cf043
ishaanyadav007 <yadav.ishaan@gmail.com>
2026-06-18 19:39:34 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:\
+        #bruteforce: count no of occurences of each element and #then check condition
+        n=len(nums)
+        # for i in range(n):
+        #     count=0
+        #     for j in range(n):
+        #         if nums[j] == nums[i]:
+        #             count+=1
+        #         if count > n/2:
+        #             return nums[i]
+        #neetcode soln for this:
+        for num in nums:
+            count  = sum(1 for i in nums if i==num)
+            if count > n/2:
+                return num
+        # hash map method:
+        # hash={}
+        # for i in range(n):
+        #     hash[nums[i]] = hash.get(nums[i],0) + 1
+        # for key,val in hash.items():
+        #     if val > n/2:
+        #         return key
